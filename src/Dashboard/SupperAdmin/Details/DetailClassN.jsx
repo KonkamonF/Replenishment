2ff93f588c8e2a8932fa6c3dee6cffe52c29c254
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-17 17:49:12 +0700

    color3

--- src/Dashboard/SupperAdmin/Details/DetailClassN.jsx
+++ src/Dashboard/SupperAdmin/Details/DetailClassN.jsx
@@ -8,14 +8,14 @@ export default function DetailClassN({ setIsDetailsClassN }) {
   ];
   return (
     <>
-      <div className="bg-amber-50 w-screen h-screen absolute mx-auto z-0 right-0 top-0 text-black">
+      <div className="bg-white w-screen h-screen absolute mx-auto z-0 right-0 top-0 text-black">
         <div
           onClick={() => setIsDetailsClassN(false)}
           className=" text-end px-6 text-3xl cursor-pointer"
         >
           x
         </div>
-        <p className="text-3xl font-bold py-4">Class N</p>
+        <p className="text-3xl font-bold py-4 text-[#640037]">Class N</p>
 
         <div>
           {" "}
