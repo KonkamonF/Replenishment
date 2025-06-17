d17ee7d142ad02620fb8eac1fa9ac7ef8a6ad930
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-17 17:07:36 +0700

    nonmove

--- src/Dashboard/SupperAdmin/Amount/NonMove.jsx
+++ src/Dashboard/SupperAdmin/Amount/NonMove.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import DetailNonMove from "../Details/DetailNonMove";
 
 export default function NonMove() {
+  const [isDetailsNonMove, setIsDetailsNonMove] = useState(false);
   return (
-    <div className="border-2 bg-[#640037] p-2 rounded-3xl w-[40%] hover:bg-gray-500">
-      <p className="text-3xl font-bold">1 Units</p>Non-Move
-    </div>
+    <>
+      {isDetailsNonMove && (
+        <DetailNonMove setIsDetailsNonMove={setIsDetailsNonMove} />
+      )}
+      <div
+        onClick={() => setIsDetailsNonMove(true)}
+        className="border-2 bg-[#640037] p-2 rounded-3xl w-[40%] hover:bg-gray-500"
+      >
+        <p className="text-3xl font-bold">1 Units</p>Non-Move
+      </div>
+    </>
   );
 }
