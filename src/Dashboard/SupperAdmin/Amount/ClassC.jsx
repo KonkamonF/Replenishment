127b11da5c46946700e3b0617b64458f91299a30
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-17 16:44:58 +0700

    classc

--- src/Dashboard/SupperAdmin/Amount/ClassC.jsx
+++ src/Dashboard/SupperAdmin/Amount/ClassC.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import DetailClassC from "../Details/DetailClassC";
 
 export default function ClassC() {
+  const [isDetailsClassC, setIsDetailsClassC] = useState(false);
   return (
-    <div className="border-2 bg-[#640037] p-2 rounded-3xl w-[40%] hover:bg-gray-500">
-      <p className="text-3xl font-bold">20 Units</p>Class C
-    </div>
+    <>
+      {isDetailsClassC && (
+        <DetailClassC setIsDetailsClassC={setIsDetailsClassC} />
+      )}
+      <div
+        onClick={() => setIsDetailsClassC(true)}
+        className="border-2 bg-[#640037] p-2 rounded-3xl w-[40%] hover:bg-gray-500"
+      >
+        <p className="text-3xl font-bold">20 Units</p>Class C
+      </div>
+    </>
   );
 }
