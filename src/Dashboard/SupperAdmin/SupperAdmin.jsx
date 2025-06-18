28a4e6bbd0d38e768d54a95b036c5ec2e11fdea0
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-18 11:40:34 +0700

    layout

--- src/Dashboard/SupperAdmin/SupperAdmin.jsx
+++ src/Dashboard/SupperAdmin/SupperAdmin.jsx
@@ -11,28 +11,28 @@ export default function SupperAdmin() {
   return (
     <div className="bg-white text-white h-screen flex flex-col p-4 space-y-4 text-center">
       <div className="flex gap-4">
-        <div className="w-1/2 flex justify-end">
+        <div className="w-[50%]">
           <ClassA />
         </div>
-        <div className="w-1/2">
+        <div className="w-[50%]">
           <ClassB />
         </div>
       </div>
 
       <div className="flex gap-4">
-        <div className="w-1/2 flex justify-end">
+        <div className="w-[50%]">
           <ClassC />
         </div>
-        <div className="w-1/2">
+        <div className="w-[50%]">
           <ClassN />
         </div>
       </div>
 
       <div className="flex gap-4">
-        <div className="w-1/2 flex justify-end">
+        <div className="w-[50%]">
           <Best />
         </div>
-        <div className="w-1/2">
+        <div className="w-[50%]">
           <NonMove />
         </div>
       </div>
