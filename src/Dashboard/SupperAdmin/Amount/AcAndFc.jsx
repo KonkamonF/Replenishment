bbe8babe7793429f8363ef582d45377ff27b0b3d
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-18 09:33:24 +0700

    chart 1

--- src/Dashboard/SupperAdmin/Amount/AcAndFc.jsx
+++ src/Dashboard/SupperAdmin/Amount/AcAndFc.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Chart as ChartJS } from "chart.js/auto";
-import { Pie } from "react-chartjs-2";
+import { Pie, Bar, Doughnut } from "react-chartjs-2";
 
 export default function AcAndFc() {
   return (
     <>
-      <div className="text-black w-[20%]">
+      <div className="text-black w-[20%] mx-auto">
         Actual เทียบ Forecast
-        <Pie
+        <Doughnut
           data={{
-            labels: ["A", "B"],
-            datasets: [{ label: "Actual เทียบ Forecast", data: [5000, 2500] }],
+            labels: ["Actual", "Forecast"],
+            datasets: [
+              {
+                label: "Actual เทียบ Forecast",
+                data: [5000, 7000],
+                backgroundColor: ["#3282B8", "#790252"],
+                borderRadius: 10,
+              },
+            ],
           }}
         />
       </div>
