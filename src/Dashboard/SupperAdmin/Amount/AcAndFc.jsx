acf9fb0e2bc0c10adddc94d0f1c7b9d7a14f300e
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-18 10:36:45 +0700

    chrat

--- src/Dashboard/SupperAdmin/Amount/AcAndFc.jsx
+++ src/Dashboard/SupperAdmin/Amount/AcAndFc.jsx
@@ -5,7 +5,7 @@ import { Pie, Bar, Doughnut } from "react-chartjs-2";
 export default function AcAndFc() {
   return (
     <>
-      <div className="text-black w-[20%] mx-auto">
+      <div className="text-[#640037] w-[25%] mx-auto text-3xl font-bold">
         Actual เทียบ Forecast
         <Doughnut
           data={{
@@ -13,7 +13,7 @@ export default function AcAndFc() {
             datasets: [
               {
                 label: "Actual เทียบ Forecast",
-                data: [5000, 7000],
+                data: [5000, 7000], //Mapping Data and Labels https://www.youtube.com/watch?v=6q5d3Z1-5kQ
                 backgroundColor: ["#3282B8", "#790252"],
                 borderRadius: 10,
               },
