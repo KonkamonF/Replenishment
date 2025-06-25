6b12656f59e2729482cf7641a106dd06e728af90
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-25 10:58:51 +0700

    key acc

--- src/Dashboard/KeyAdmin/KeyAdmin.jsx
+++ src/Dashboard/KeyAdmin/KeyAdmin.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 
 export default function KeyAdmin() {
   return (
-    <div>KeyAdmin</div>
+     <div className="bg-white text-[#640037] h-screen flex flex-col text-center">
+      <div className="flex gap-12">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+        </div>
+        <p>...</p>
+      </div>
+    </div>
   )
 }
