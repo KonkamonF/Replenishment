50b13b3ae5d427231b004f0b0303661d1b8ad1ff
KonkamonF <konkamonfungsuk@gmail.com>
2025-06-25 10:27:30 +0700

    avgsaleout

---
+++ src/Dashboard/TradeAdmin/AmountTrade/AvgSaleOut.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AvgSaleOut() {
+  return (
+    <>
+    <div>ยอดตัดจ่าย</div>
+    </>
+  )
+}
--- src/Dashboard/TradeAdmin/TradeAdmin.jsx
+++ src/Dashboard/TradeAdmin/TradeAdmin.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import AvgSaleOut from "./AmountTrade/AvgSaleOut";
 
 export default function TradeAdmin() {
   return (
-    <div className='bg-white'>TradeAdmin</div>
-  )
+    <div className="bg-white text-[#640037] h-screen flex flex-col text-center">
+      <div className="flex gap-12">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <AvgSaleOut />
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+
+          <div className="flex gap-8">
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+            <div className="w-[200px]">
+              <p>...</p>
+            </div>
+          </div>
+        </div>
+        <p>...</p>
+      </div>
+    </div>
+  );
 }
