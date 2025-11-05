19b8be56b52db5dbb1908aa4a3f6abdcaad5cbe6
kran <kran.sbo.fun@gmail.com>
2025-11-05 17:40:18 +0700

    aw

--- src/Dashboard/SupperAdmin/DetailsAdmin/DetailClassA.jsx
+++ src/Dashboard/SupperAdmin/DetailsAdmin/DetailClassA.jsx
@@ -150,7 +150,7 @@ export default function DetailClassA({ setIsDetailsClassA }) {
                 ← หน้าก่อนหน้า
               </button>
               <span className="text-gray-600 mt-1">
-                หน้า {page}
+                หน้า {page} 
               </span>
               <button
                 disabled={products.length < offset}
